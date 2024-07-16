09806384dea4b060002937be6509726836f9cb44
Sorokovsky <Sorokovskys@ukr.net>
2024-07-16 15:08:25 +0300

    user settings item component style

--- src/components/settings-list/setting-item/setting-item.tsx
+++ src/components/settings-list/setting-item/setting-item.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { FC, memo } from "react";
 import { ISettingItem } from "./setting-item.interface";
+import styles from "./setting-item.module.sass"
+import cn from "classnames"
 
 const SettingItem: FC<ISettingItem> = ({item}): JSX.Element => {
     return (
-        <Link href={item.path}>
+        <Link className={cn(styles.link)} href={item.path}>
             <item.Icon />
             <h3>{item.title}</h3>
         </Link>
