75c3183b0d43f1cb5566594487c0dd75d10578f6
Sorokovsky <Sorokovskys@ukr.net>
2024-07-17 16:56:30 +0300

    start input ui

--- src/components/ui/form/input/input.interface.ts
+++ src/components/ui/form/input/input.interface.ts
@@ -1,7 +1,9 @@
 import { HTMLInputTypeAttribute } from "react";
+import { IconType } from "react-icons";
 
 export interface IInput {
     type?: HTMLInputTypeAttribute;
     placeholder?: string;
     className?: string;
+    Icon?: IconType;
 };
