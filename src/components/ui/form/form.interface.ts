63cbc1f4beb590fdc958ac2484e8d874612b8418
Sorokovsky <Sorokovskys@ukr.net>
2024-07-17 16:45:49 +0300

    start form

---
+++ src/components/ui/form/form.interface.ts
@@ -0,0 +1,6 @@
+import { FormEvent, ReactNode } from "react";
+
+export interface IForm {
+    children: ReactNode;
+    onSubmit: (ev: FormEvent<HTMLFormElement>) => void;
+}
---
+++ src/components/ui/form/form.tsx
@@ -0,0 +1,12 @@
+import { FC, memo } from "react";
+import { IForm } from "./form.interface";
+
+const Form: FC<IForm> = (): JSX.Element => {
+    return (
+        <form>
+            
+        </form>
+    );
+};
+
+export default memo(Form);
