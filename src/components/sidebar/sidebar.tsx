f338c4b8a68a593da9eaafb2fa8bd2a263361283
Sorokovsky <Sorokovskys@ukr.net>
2024-07-18 12:46:14 +0300

    sidebar showing start fix

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,20 +1,25 @@
-import { FC, memo, MouseEventHandler, useEffect, useRef } from "react";
+import { FC, memo, MouseEvent, MouseEventHandler, useEffect, useRef } from "react";
 import { ISidebar } from "./sidebar.interface";
 import styles from "./sidebar.module.sass";
 import cn from "classnames";
 import { userSettings } from "@/consts/user-settings/user-settings";
 import SettingsList from "@/components/settings-list/settings-list";
-const Sidebar: FC<ISidebar> = ({isOpen, position = 'right'}): JSX.Element => {
+const Sidebar: FC<ISidebar> = ({isOpen, position = 'right', close}): JSX.Element => {
     const isOpenClass = styles.isOpen;
     const body = useRef<HTMLElement>();
-    const clickHandler: MouseEventHandler = (ev) => {
-
+    const clickHandler = (ev: globalThis.MouseEvent) => {
+        ev.stopPropagation();
+        
+        
     }
 
     useEffect(() => {
         body.current = document.body;
-        body.current.addEventListener("click", clickHandler;
-    }, []
+        body.current?.addEventListener("click", clickHandler);
+        return () => {
+            body?.current?.removeEventListener("click", clickHandler);
+        }
+    }, [body.current]
     );
     return (
         <aside
