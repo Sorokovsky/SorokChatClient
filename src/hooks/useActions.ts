58a8641e2d4fc32d610e0cd0707fec1424846c36
Андрій Сороковський <andrijsorokovskij@MacBook-Air-Andrij.local>
2024-07-20 18:38:29 +0300

    refactor

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -3,12 +3,10 @@ import { bindActionCreators } from "@reduxjs/toolkit";
 import { userActions } from "@/store/slices/user/user";
 import { asyncActions } from "@/store/store";
 import { useDispatch } from "react-redux";
-import { useAppDispatch } from "@/store/store";
 
 export const useActions = () => {
     const actions = {...settingsActions, ...userActions};
     const dispatch = useDispatch();
-    const appDispatch = useAppDispatch();
     return {
         ...bindActionCreators(actions, dispatch),
         ...bindActionCreators(asyncActions, dispatch)
