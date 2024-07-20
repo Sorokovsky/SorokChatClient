c2860a6dcdc9fb854a2e4b6322acfa0a3517c2a8
Sorokovsky <Sorokovskys@ukr.net>
2024-07-20 22:26:24 +0300

    register ui

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,22 +1,26 @@
 import axios from "axios";
 
-axios.interceptors.response.use(function (response) {
-    const auth = response.headers["Authorization"];
-    if(auth) {
-        auth.split(" ");
-        if(auth.length === 2) {
-            saveAccessToken(auth[1]);
-        }
-    }
-    return response;
-}, function (error) {
-    return Promise.reject(error);
-});
-
-export const getInstanse = () => axios.create({
+export const getInstanse = () => {
+    const instanse = axios.create({
     baseURL: process.env.API,
     headers: {"Authorization": `Bearer ${localStorage.getItem("access_token") || ""}`}
-});
+    });
+    instanse.interceptors.response.use(function (response) {
+        const auth = response.headers["Authorization"];
+        console.log('auth');
+        if(auth) {
+            
+            let parts = auth.split(" ");
+            if(parts.length === 2) {
+                saveAccessToken(auth[1]);
+            }
+        }
+        return response;
+    }, function (error) {
+        return Promise.reject(error);
+    });
+    return instanse;
+}
 
 const saveAccessToken = (token: string) => {
     localStorage.setItem("access_token", token);
