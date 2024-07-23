9c3f51a3f9103c9a42f2878622b853411751cb13
Андрій Сороковський <andrijsorokovskij@MacBook-Air-Andrij.local>
2024-07-23 19:52:44 +0300

    fix

--- src/utils/prepare-user-to-fileds.ts
+++ src/utils/prepare-user-to-fileds.ts
@@ -5,18 +5,18 @@ import { TUser } from "@/types/user.type";
 export const prepareUserToFields = (user: TUser | null): IField[] => {
     if(user === null) return [];
     const defaultAvatar = "https://static-00.iconduck.com/assets.00/user-avatar-icon-2048x2048-wpp8os2d.png";
-    const avatar = (user.avatarPath.trim() !== '' && user.avatarPath !== undefined) ? user.avatarPath : defaultAvatar;
+    const avatar = (user.avatarPath.trim() !== '' && user.avatarPath !== undefined) ? useAvatar(user.avatarPath) : defaultAvatar;
     let keys: (keyof TUser)[] = Object.keys(user) as (keyof TUser)[];
     keys = keys.filter(key => key !== 'avatarPath' && key !== 'createdAt' && key !== 'updatedAt' && key !== 'id');
-    return [...keys.map(key => ({
+    return [{
+        placeholder: "",
+        slug: 'avatar',
+        type: "file",
+        image: avatar,
+    }, ...keys.map(key => ({
         slug: key,
         placeholder: key!,
         defaultValue: user[key]!,
         type: 'text',
-    })), {
-        placeholder: "",
-        slug: 'avatar',
-        type: "file",
-        image: useAvatar(avatar),
-    },];
+    }))];
 }
