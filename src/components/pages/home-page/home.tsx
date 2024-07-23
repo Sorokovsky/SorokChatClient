5489da4fb2ac9ca08c93471bcdb7296db02213d3
Sorokovsky <Sorokovskys@ukr.net>
2024-07-23 10:58:56 +0300

    update logic

--- src/components/pages/home-page/home.tsx
+++ src/components/pages/home-page/home.tsx
@@ -3,13 +3,24 @@ import { NextPage } from "next";
 import styles from './index.module.sass';
 import Search from "@/ui/search/search"
 import cn from "classnames";
-import { ChangeEvent, useCallback } from "react";
+import { ChangeEvent, useCallback, useEffect } from "react";
+import { useAppSelector } from "@/store/store"
+import { useRouter } from "next/navigation";
 
 const HomePage: NextPage = (): JSX.Element => {
     const searchHandler = useCallback((ev: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         }, 
         []
     );
+
+    const { user } = useAppSelector(state => state.user);
+    const { push } = useRouter();
+    useEffect(() => {
+        if(user === null) {
+            push("/login");
+        }
+    }, [user]);
+
     return (
         <>
             <section className={cn(styles.page)}>
